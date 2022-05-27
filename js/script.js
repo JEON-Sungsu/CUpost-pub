72c76e424e3bf2d8e3cf23a141a846004b6c33e7
JEON Sungsu <anagma@sjsofttech.co.kr>
2022-05-27 22:21:33 +0900

    0526. 메인페이지 추가

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
+
+
 $(document).ready(function(){
     
-    // 스크롤 탑-모바일
-    var mybutton2 = document.getElementById("myBtn-mob");
-    window.onscroll = function() {scrollFunction()};
-    // function scrollFunction() {
-    //     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    //         mybutton2.style.display = "block";
-    //     } else {
-    //         mybutton2.style.display = "none";
-    //     }
-    // }
-    function topFunctionMob() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
-    
+
     $('.checking-input').on('keydown',function(){
         $(this).next('.input-btn-left').css('opacity',1)
     })
@@ -23,6 +11,47 @@ $(document).ready(function(){
         console.log(this)
     })
 
-    
+    os = mobileOs();
+
+    if(os == "android"){
+        $("body").addClass("android");
+    }
+    else if(os == "ios"){
+        //$("body").addClass("ios");
+    }
+
+   
 });
 
+// android, ios 체크
+function mobileOs(){
+    var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+    if ( varUA.indexOf('android') > -1) {
+        //안드로이드
+        return "android";
+    } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+        //IOS
+        return "ios";
+    }
+    return "local";
+}
+
+    // var window = $(window);
+	var body = $('body');
+    var mybutton = document.getElementById("myBtn-text");
+    var mybutton2 = document.getElementById("myBtn-mob");
+    
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            mybutton.style.display = "block";
+            mybutton2.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+            mybutton2.style.display = "none";
+        }
+    }
+    function topFunctionMob() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
